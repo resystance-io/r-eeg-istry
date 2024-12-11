7c6ce6cb6648ad08257e7b984ec6908ef8e63019
Daniel Haslinger <creolis@users.noreply.github.com>
2024-12-11 16:43:56 +0100

    avoid pushing actual credentials to the repository by accident

--- configs/config.php
+++
@@ -1,2 +0,0 @@
-// Content from config.template.php goes here
-// This file is just a placeholder until deployed
--- configs/config.template.php
+++ configs/config.template.php
@@ -2,17 +2,15 @@
 
 /////////* DATABASE *//////////////
 
-	$DBLOGON_HOST           =   '';
-	$DBLOGON_USER           =   '';
-	$DBLOGON_PASS           =   '';
-	$DBLOGON_DB             =   '';
+$DBLOGON_HOST           =   '';
+$DBLOGON_USER           =   '';
+$DBLOGON_PASS           =   '';
+$DBLOGON_DB             =   '';
 
 /////////* DATABASE TABLE MAPPINGS *///////////
 
-	$DBTABLE_EXAMPLE		= 	'example';
+$DBTABLE_EXAMPLE		= 	'example';
 
-	// shall we print debug messages?
-	// (enabling this in production is a hell of a bad idea)
-	$debug                  =   false;
-
-?>
+// shall we print debug messages?
+// (enabling this in production is a hell of a bad idea)
+$debug                  =   false;
