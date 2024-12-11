8271a2bd2edc4b0892a2c6923b3136602346fdf0
Daniel Haslinger <creolis@users.noreply.github.com>
2024-12-11 16:34:08 +0100

    added config template

--- configs/config.php
+++ configs/config.php
@@ -1,18 +1,2 @@
-<?php
-
-/////////* DATABASE *//////////////
-
-	$DBLOGON_HOST           =   '';
-	$DBLOGON_USER           =   '';
-	$DBLOGON_PASS           =   '';
-	$DBLOGON_DB             =   '';
-
-/////////* DATABASE TABLE MAPPINGS *///////////
-
-	$DBTABLE_EXAMPLE		= 	'example';
-
-	// shall we print debug messages?
-	// (enabling this in production is a hell of a bad idea)
-	$debug                  =   false;
-
-?>
+// Content from config.template.php goes here
+// This file is just a placeholder until deployed
---
+++ configs/config.template.php
@@ -0,0 +1,18 @@
+<?php
+
+/////////* DATABASE *//////////////
+
+	$DBLOGON_HOST           =   '';
+	$DBLOGON_USER           =   '';
+	$DBLOGON_PASS           =   '';
+	$DBLOGON_DB             =   '';
+
+/////////* DATABASE TABLE MAPPINGS *///////////
+
+	$DBTABLE_EXAMPLE		= 	'example';
+
+	// shall we print debug messages?
+	// (enabling this in production is a hell of a bad idea)
+	$debug                  =   false;
+
+?>
