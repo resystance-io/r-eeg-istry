13806e26504162c5feaf6ebb4753641472ef454d
Daniel Haslinger <creolis@users.noreply.github.com>
2024-12-18 11:30:15 +0100

    complete re-write of meter handling code
    all meters are now in the same bucket and store their type as metadata. Added additional buckets for generic information etc. and implemented store/load for all form fields

--- views/view_join.inc.php
+++ views/view_join.inc.php
@@ -6,14 +6,17 @@ function view_render()
     switch($_REQUEST['join'])
     {
         case "individual":
+            $_SESSION['generic_information']['join_type'] = "individual";
             view_render_individual();
             break;
 
         case "agriculture":
+            $_SESSION['generic_information']['join_type'] = "agriculture";
             view_render_agriculture();
             break;
 
         case "company":
+            $_SESSION['generic_information']['join_type'] = "company";
             view_render_company();
             break;
 
@@ -59,13 +62,13 @@ function view_render_company()
     print "<h3>Allgemeine Daten</h3>";
     print "<div class=\"form-container\">";
 
-    view_render_part_captioned_inputfield("Firmenwortlaut", "company", "company");
-    view_render_part_captioned_inputfield("UID", "uid", "uid");
-    view_render_part_captioned_inputfield("Postleitzahl", "zip", "zip");
-    view_render_part_captioned_inputfield("Ort", "city", "city");
-    view_render_part_captioned_inputfield("Stra&szlig;e", "street", "street");
-    view_render_part_captioned_inputfield("Hausnummer", "number", "number");
-    view_render_part_captioned_inputfield("Telefonnummer", "phone", "phone");
+    view_render_part_captioned_inputfield("Firmenwortlaut", "company", "generic_information");
+    view_render_part_captioned_inputfield("UID", "uid", "generic_information");
+    view_render_part_captioned_inputfield("Postleitzahl", "zip", "generic_information");
+    view_render_part_captioned_inputfield("Ort", "city", "generic_information");
+    view_render_part_captioned_inputfield("Stra&szlig;e", "street", "generic_information");
+    view_render_part_captioned_inputfield("Hausnummer", "number", "generic_information");
+    view_render_part_captioned_inputfield("Telefonnummer", "phone", "generic_information");
 
     print "</div><br />";
 
@@ -91,15 +94,15 @@ function view_render_individual()
     print "<h3>Allgemeine Daten</h3>";
     print "<div class=\"form-container\">";
 
-    view_render_part_captioned_inputfield("Vorname", "firstname", "firstname");
-    view_render_part_captioned_inputfield("Nachname", "lastname", "lastname");
-    view_render_part_captioned_inputfield("Stra&szlig;e", "street", "street");
-    view_render_part_captioned_inputfield("Hausnummer", "number", "number");
-    view_render_part_captioned_inputfield("Postleitzahl", "zip", "zip");
-    view_render_part_captioned_inputfield("Ort", "city", "city");
-    view_render_part_captioned_inputfield("Geburtsdatum", "birthdate", "birthdate");
-    view_render_part_captioned_inputfield("Telefonnummer", "phone", "phone");
-    view_render_part_captioned_inputfield("E-Mail Adresse", "email", "email");
+    view_render_part_captioned_inputfield("Vorname", "firstname", "generic_information");
+    view_render_part_captioned_inputfield("Nachname", "lastname", "generic_information");
+    view_render_part_captioned_inputfield("Stra&szlig;e", "street", "generic_information");
+    view_render_part_captioned_inputfield("Hausnummer", "number", "generic_information");
+    view_render_part_captioned_inputfield("Postleitzahl", "zip", "generic_information");
+    view_render_part_captioned_inputfield("Ort", "city", "generic_information");
+    view_render_part_captioned_inputfield("Geburtsdatum", "birthdate", "generic_information");
+    view_render_part_captioned_inputfield("Telefonnummer", "phone", "generic_information");
+    view_render_part_captioned_inputfield("E-Mail Adresse", "email", "generic_information");
 
     print "</div><br />";
 
@@ -125,15 +128,15 @@ function view_render_agriculture()
     print "<h3>Allgemeine Daten</h3>";
     print "<div class=\"form-container\">";
 
-    view_render_part_captioned_inputfield("Vorname", "firstname", "firstname");
-    view_render_part_captioned_inputfield("Nachname", "lastname", "lastname");
-    view_render_part_captioned_inputfield("Stra&szlig;e", "street", "street");
-    view_render_part_captioned_inputfield("Hausnummer", "number", "number");
-    view_render_part_captioned_inputfield("Postleitzahl", "zip", "zip");
-    view_render_part_captioned_inputfield("Ort", "city", "city");
-    view_render_part_captioned_inputfield("Geburtsdatum", "birthdate", "birthdate");
-    view_render_part_captioned_inputfield("Telefonnummer", "phone", "phone");
-    view_render_part_captioned_inputfield("E-Mail Adresse", "email", "email");
+    view_render_part_captioned_inputfield("Vorname", "firstname", "generic_information");
+    view_render_part_captioned_inputfield("Nachname", "lastname", "generic_information");
+    view_render_part_captioned_inputfield("Stra&szlig;e", "street", "generic_information");
+    view_render_part_captioned_inputfield("Hausnummer", "number", "generic_information");
+    view_render_part_captioned_inputfield("Postleitzahl", "zip", "generic_information");
+    view_render_part_captioned_inputfield("Ort", "city", "generic_information");
+    view_render_part_captioned_inputfield("Geburtsdatum", "birthdate", "generic_information");
+    view_render_part_captioned_inputfield("Telefonnummer", "phone", "generic_information");
+    view_render_part_captioned_inputfield("E-Mail Adresse", "email", "generic_information");
 
     print "</div><br />";
 
@@ -149,16 +152,28 @@ function view_render_consumption_meters()
     print "<h3>Z&auml;hlpunkte (Bezug)</h3>";
     print "<div class=\"form-container\">";
 
-    if(isset($_SESSION['consumers']))
+    if(isset($_SESSION['meters']))
     {
-        foreach($_SESSION['consumers'] as $consumer_key => $consumer_value)
+        $consumer_count = 0;
+        foreach($_SESSION['meters'] as $meter_key => $meter_object)
         {
-            $display_key = $consumer_key + 1;
-            view_render_part_captioned_prefixed_inputfield("$display_key. Z&auml;hlpunktnummer (letzte 9 Stellen)", "AT003000000000000000000003", "consume0", "consume0", $consumer_value);
+            if($meter_object['type'] == "consumers")
+            {
+                view_render_part_captioned_prefixed_inputfield("Z&auml;hlpunktnummer (letzte 9 Stellen)", "AT003000000000000000000000", $meter_key, $meter_object['value']);
+                $consumer_count++;
+            }
         }
+
+        print "<div id='end_of_consumers'></div>";
     }
 
-    print '<br /><i style="font-size:16px" class="icon fa-plus-square" onclick="JaxonInteractives.add_consume_meter();"></i><span class="label" style="font-weight:normal;font-size:16px" onclick="JaxonInteractives.add_consume_meter();">&nbsp; Einen weiteren Bezugsz&auml;hlpunkt hinzuf&uuml;gen</span>';
+    if(!isset($_SESSION['meters']) || $consumer_count == 0)
+    {
+        $id = generate_uuid4();
+        view_render_part_captioned_prefixed_inputfield("Z&auml;hlpunktnummer (letzte 9 Stellen)", "AT003000000000000000000000", $id);
+    }
+
+    print '<br /><i style="font-size:16px;cursor:pointer;" class="icon fa-plus-square" onclick="JaxonInteractives.add_meter(' . "'consumers'" . ',' . "'AT003000000000000000000000'" . ');"></i><span class="label" style="font-weight:normal;font-size:16px;cursor:pointer;" onclick="JaxonInteractives.add_meter(' . "'consumers'" . ',' . "'AT003000000000000000000000'" . ');">&nbsp; Einen Bezugsz&auml;hlpunkt hinzuf&uuml;gen</span>';
     print "</div>";
 }
 
@@ -167,27 +182,73 @@ function view_render_supply_meters()
     print "<h3>Z&auml;hlpunkte (Einspeisung)</h3>";
     print "<div class=\"form-container\">";
 
-    view_render_part_captioned_prefixed_inputfield("1. Z&auml;hlpunktnummer (letzte 9 Stellen)", "AT003000000000000000000003", "supply0", "supply0");
+    if(isset($_SESSION['meters']))
+    {
+        foreach($_SESSION['meters'] as $meter_key => $meter_object)
+        {
+            if($meter_object['type'] == "suppliers")
+            {
+                view_render_part_captioned_prefixed_inputfield("Z&auml;hlpunktnummer (letzte 9 Stellen)", "AT003000000000000000000003", $meter_key, $meter_object['value']);
+            }
+        }
 
-    print '<br /><i style="font-size:16px" class="icon fa-plus-square" onclick="JaxonInteractives.add_supply_meter();"></i><span class="label" style="font-weight:normal;font-size:16px" onclick="JaxonInteractives.add_supply_meter();">&nbsp; Einen weiteren Einspeisez&auml;hlpunkt hinzuf&uuml;gen</span>';
+        print "<div id='end_of_suppliers'></div>";
+    }
+
+    print '<br /><i style="font-size:16px;cursor:pointer;" class="icon fa-plus-square" onclick="JaxonInteractives.add_meter(' . "'suppliers'" . ',' . "'AT003000000000000000000003'" . ');"></i><span class="label" style="font-weight:normal;font-size:16px;cursor:pointer;" onclick="JaxonInteractives.add_meter(' . "'suppliers'" . ',' . "'AT003000000000000000000003'" . ');">&nbsp; Einen Einspeisez&auml;hlpunkt hinzuf&uuml;gen</span>';
     print "</div>";
 }
 
-function view_render_part_captioned_inputfield($caption, $name, $id)
+function view_render_part_captioned_inputfield($caption, $id, $session_bucket = null)
 {
-    print "$caption<br><input type=\"text\" name=\"$name\" id=\"$id\" /><br />";
+    if($session_bucket != null)
+    {
+        if(isset($_SESSION["$session_bucket"]["$id"]))
+        {
+            $prefill = $_SESSION["$session_bucket"]["$id"];
+        }
+        else
+        {
+            $prefill = '';
+        }
+
+        print $caption . '<br><input type="text" name="id" id="' . $id . '" value="' . $prefill . '" onfocusout="JaxonInteractives.update_session_bucket(' . "'" . $id . "'" . ', document.getElementById(' . "'" . $id . "'" . ').value, ' . "'" .  $session_bucket . "'" . ');" />';
+    }
+    else
+    {
+        print $caption . '<br><input type="text" name="id" id="' . $id . '" />';
+    }
+
+    print '<br />';
 }
 
-function view_render_part_captioned_prefixed_inputfield($caption, $prefix, $name, $id, $value="000000000")
+function view_render_part_captioned_prefixed_inputfield($caption, $prefix, $id, $value="000000000")
 {
-    //print "<input type=\"text\" name=\"$name\" id=\"$id\" class=\"prefix\" /><br />";
+
     print '
-        &nbsp;<br /><div id="container-' . $id . '">' . $caption . '<br>
+        <div id="container-' . $id . '">' . $caption . '<br>
             <div class="input-box">
                 <span class="prefix">' . $prefix . '</span>
-                <input type="text" name="' . $name . '" id="' . $id . '" value="' . $value . '" maxlength="9" onfocus="this.select()" />
+                <input type="text" name="' . $id . '" id="' . $id . '" value="' . $value . '" maxlength="9" onfocus="this.select()" onfocusout="JaxonInteractives.update_meter_value(' . "'" . $id . "'" . ', document.getElementById(' . "'" . $id . "'" . ').value);" />
+                &nbsp;&nbsp;<button style="background-color:darkred"  onclick="JaxonInteractives.rmv_meter(' . "'" . $id . "'" . ');"><i style="font-size:16px;color:white;" class="icon fa-trash-alt"></i></button><br>
             </div>
+            <br />
         </div>';
+}
+
+function generate_uuid4($data = null)
+{
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
 ?>
