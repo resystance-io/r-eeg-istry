7143e0f06f4fda4a60a9fe152d587932b38a1085
Daniel Haslinger <creolis@users.noreply.github.com>
2025-05-25 16:58:04 +0200

    Disable error reporting in index.php for compatibility
        Added `error_reporting(0)` to suppress all error messages. This ensures compatibility with servers where `php.ini` cannot be configured, preventing warnings from disrupting session setup.

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+    // Turn off all error reporting since some servers do not provide a way
+    // to manipulate their php.ini and we can't handle warnings that break our
+    // session setup:
+    error_reporting(0);
+
     include_once('views/view.inc.php');
     $view = new VIEW();
 
