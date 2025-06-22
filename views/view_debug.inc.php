28f64621ffb659678a5063cf69b56d48365fbffb
Daniel Haslinger <creolis@users.noreply.github.com>
2025-06-22 21:59:57 +0200

    minor changes

--- views/view_debug.inc.php
+++ views/view_debug.inc.php
@@ -17,6 +17,17 @@ class VIEW_DEBUG
         print "<br />&nbsp;<br />&nbsp;<br />";
         print "<h3>SESSION DUMP:</h3>";
         print "<div class=\"form-container\">";
+        print '
+            <script>
+              Dropzone.options.myDropzone = {
+                maxFilesize: "10M",
+                maxFiles: 1
+              };
+            </script>
+            <form action="?upload" class="dropzone" id="my-dropzone">
+                <div class="dz-message" data-dz-message><span style="color:dimgrey">Datei zur &Uuml;bermittlung hier ablegen oder klicken um eine Datei auszuw&auml;hlen</span></div>
+            </form>
+        ';
         print "<pre>";
         print_r($_SESSION);
         print "</pre>";
